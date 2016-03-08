d0c42e908dc3e74c3d98886e07e3b5049aae3b96
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2016-03-08 17:25:13 +0000

    Add in the strict mode directive rule

--- base.js
+++ base.js
@@ -6,6 +6,7 @@ module.exports = {
     './rules/ecmascript-6',
     './rules/node',
     './rules/possible-errors',
+    './rules/strict-mode',
     './rules/variables'
   ].map(require.resolve),
   rules: {}
