73c8220edd1f58badc03bb15c015a141f04a08a0
Jamie Connolly <jamie@jamieconnolly.com>
2016-03-08 13:39:56 +0000

    Add in tests to make sure the shared config files can be loaded

---
+++ tests/test-base.js
@@ -0,0 +1,22 @@
+import eslint from 'eslint';
+import isObject from 'is-object';
+import path from 'path';
+import test from 'tape';
+
+import config from '../base';
+
+test('test basic properties of base config', (t) => {
+  t.ok(isObject(config.extends), 'extends is an object');
+  t.ok(isObject(config.rules), 'rules is an object');
+  t.end();
+});
+
+test('load base config in eslint to validate rule syntax is correct', (t) => {
+  const cli = new eslint.CLIEngine({
+    configFile: path.join(__dirname, '..', 'base.js'),
+    useEslintrc: false
+  });
+
+  t.ok(cli.executeOnText('var foo;\n'), 'can executeOnText');
+  t.end();
+});
---
+++ tests/test-index.js
@@ -0,0 +1,22 @@
+import eslint from 'eslint';
+import isObject from 'is-object';
+import path from 'path';
+import test from 'tape';
+
+import config from '../index';
+
+test('test basic properties of default config', (t) => {
+  t.ok(isObject(config.extends), 'extends is an object');
+  t.ok(isObject(config.rules), 'rules is an object');
+  t.end();
+});
+
+test('load default config in eslint to validate rule syntax is correct', (t) => {
+  const cli = new eslint.CLIEngine({
+    configFile: path.join(__dirname, '..', 'index.js'),
+    useEslintrc: false
+  });
+
+  t.ok(cli.executeOnText('var foo;\n'), 'can executeOnText');
+  t.end();
+});
