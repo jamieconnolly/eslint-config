23d820161435d6c99e6d7b5df85acf0ecbfd98aa
Jamie Connolly <76968+jamieconnolly@users.noreply.github.com>
2016-03-08 13:39:56 +0000

    Add in tests to make sure the shared config files can be loaded

---
+++ tests/test-legacy.js
@@ -0,0 +1,25 @@
+import eslint from 'eslint';
+import isObject from 'is-object';
+import path from 'path';
+import test from 'tape';
+
+import config from '../legacy';
+
+test('test basic properties of legacy config', (t) => {
+  t.ok(isObject(config.ecmaFeatures), 'ecmaFeatures is an object');
+  t.ok(isObject(config.env), 'env is an object');
+  t.ok(isObject(config.extends), 'extends is an object');
+  t.ok(isObject(config.globals), 'globals is an object');
+  t.ok(isObject(config.rules), 'rules is an object');
+  t.end();
+});
+
+test('load legacy config in eslint to validate rule syntax is correct', (t) => {
+  const cli = new eslint.CLIEngine({
+    configFile: path.join(__dirname, '..', 'legacy.js'),
+    useEslintrc: false
+  });
+
+  t.ok(cli.executeOnText('var foo;\n'), 'can executeOnText');
+  t.end();
+});
