65d7f2ec1d04e8480f2afe295d5caf8f984d2396
Jamie Connolly <jamie@jamieconnolly.com>
2017-02-27 21:58:59 +0000

    :hammer: Refactor package down to ES5

--- tests/test-base.js
+++ tests/test-base.js
@@ -1,18 +1,20 @@
-import config from '../src/base';
-import eslint from 'eslint';
-import isObject from 'is-object';
-import path from 'path';
-import test from 'tape';
+'use strict';
 
-test('test basic properties of base config', (t) => {
+var config = require('../lib/base'),
+    eslint = require('eslint'),
+    isObject = require('is-object'),
+    path = require('path'),
+    test = require('tape');
+
+test('test basic properties of base config', function(t) {
   t.ok(isObject(config.extends), 'extends is an object');
   t.ok(isObject(config.rules), 'rules is an object');
   t.end();
 });
 
-test('load base config in eslint to validate rule syntax is correct', (t) => {
-  const cli = new eslint.CLIEngine({
-    configFile: path.join(__dirname, '../src/base.js'),
+test('load base config in eslint to validate rule syntax is correct', function(t) {
+  var cli = new eslint.CLIEngine({
+    configFile: path.join(__dirname, '../lib/base.js'),
     useEslintrc: false,
   });
 
--- tests/test-legacy.js
+++ tests/test-legacy.js
@@ -1,18 +1,20 @@
-import config from '../src/legacy';
-import eslint from 'eslint';
-import isObject from 'is-object';
-import path from 'path';
-import test from 'tape';
+'use strict';
 
-test('test basic properties of legacy config', (t) => {
+var config = require('../lib/legacy'),
+    eslint = require('eslint'),
+    isObject = require('is-object'),
+    path = require('path'),
+    test = require('tape');
+
+test('test basic properties of legacy config', function(t) {
   t.ok(isObject(config.extends), 'extends is an object');
   t.ok(isObject(config.rules), 'rules is an object');
   t.end();
 });
 
-test('load legacy config in eslint to validate rule syntax is correct', (t) => {
-  const cli = new eslint.CLIEngine({
-    configFile: path.join(__dirname, '../src/legacy.js'),
+test('load legacy config in eslint to validate rule syntax is correct', function(t) {
+  var cli = new eslint.CLIEngine({
+    configFile: path.join(__dirname, '../lib/legacy.js'),
     useEslintrc: false,
   });
 
