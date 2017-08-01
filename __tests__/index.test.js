f5bb691f53663a692f7e37367bbc5b3e7f2a9a17
Jamie Connolly <jamie@jamieconnolly.com>
2017-08-01 22:02:12 +0100

    :hammer: Refactor tests to be more jest-like

---
+++ __tests__/index.test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const eslint = require('eslint');
+const isPlainObj = require('is-plain-obj');
+
+it('should export an object', function() {
+  const config = require('../index');
+
+  expect(isPlainObj(config)).toBe(true);
+  expect(config.extends).toBeTruthy();
+  expect(config.parser).toBeTruthy();
+});
+
+it('should not contain invalid rules', function() {
+  const cli = new eslint.CLIEngine({
+    configFile: 'index.js',
+    useEslintrc: false,
+  });
+
+  const report = cli.executeOnText(
+    'export default [];\n'
+  );
+
+  expect(report).toBeTruthy();
+  expect(report.errorCount).toBe(0);
+  expect(report.warningCount).toBe(0);
+});
